e3642168eb81b0697d6401fe71469d4de2886d2a
ysd <ysdysd01@163.com>
2020-08-01 11:26:23 +0800

    初始化系统

---
+++ src/components/Navigator.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class='app-home-navigator'>
+         <h1>头条</h1>
+    <section class="nav-list">
+        <section 
+            v-for="item in navs"
+            :key="item.id"
+        class="nav-item">{{item.text}}</section>
+    </section>
+  </div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+//import引入的组件需要注入到对象中才能使用
+components: {},
+data() {
+//这里存放数据
+    return {
+  navs: [
+        { id: 1, text: "推荐" },
+        { id: 2, text: "西瓜视频" },
+        { id: 3, text: "财经" },
+        { id: 4, text: "热点" },
+        { id: 5, text: "直播" },
+        { id: 6, text: "图片" },
+        { id: 7, text: "科技" },
+        { id: 8, text: "娱乐" },
+        { id: 9, text: "游戏" },
+        { id: 10, text: "体育" },
+        { id: 11, text: "懂车帝" },
+        { id: 12, text: "热点" },
+      ],
+    };
+  },
+//监听属性 类似于data概念
+computed: {},
+//监控data中的数据变化
+watch: {},
+//方法集合
+methods: {
+
+},
+//生命周期 - 创建完成（可以访问当前this实例）
+created() {},
+//生命周期 - 挂载完成（可以访问DOM元素）
+mounted() {},
+//生命周期 - 创建之前
+beforeCreate() {},
+//生命周期 - 挂载之前
+beforeMount() {},
+ //生命周期 - 更新之前
+beforeUpdate() {},
+//生命周期 - 更新之后
+updated() {},
+//生命周期 - 销毁之前
+beforeDestroy() {},
+//生命周期 - 销毁完成
+destroyed() {},
+//如果页面有keep-alive缓存功能，这个函数会触发
+activated() {},
+}
+</script>
+
+<style lang='less' scoped>
+.app-home-navigator {
+  h1 {
+      text-align: center;
+      color:#ea4245;
+      font-size:20px;
+      font-weight: 100;
+  }
+
+  .nav-list {
+    .nav-item {
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+        transition: all .3s;
+    }
+    .nav-item:hover{
+        background-color:#ea4245 ;
+        border-radius: 5px;
+        color:white;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,92 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class>
+      <!-- 主页header的开始 -->
+    <div class="tt-home-header">
+      <span>头条</span>
+    </div>
+    <!-- 主页header的结束 -->
+    <!-- 主页内容开始 -->
+    <div class="tt-home-content">
+      <div class="home-content-left">
+        <Navigator></Navigator>
+      </div>
+      <div class="home-content-middle">中间</div>
+      <div class="home-content-right">右边</div>
+    </div>
+    <!-- 主页内容结束 -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+import Navigator from "../components/Navigator"
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
+//import引入的组件需要注入到对象中才能使用
+components: {
+  Navigator
+},
+data() {
+//这里存放数据
+  return {
+
+  };
+},
+//监听属性 类似于data概念
+computed: {},
+//监控data中的数据变化
+watch: {},
+//方法集合
+methods: {
+
+},
+//生命周期 - 创建完成（可以访问当前this实例）
+created() {},
+//生命周期 - 挂载完成（可以访问DOM元素）
+mounted() {},
+//生命周期 - 创建之前
+beforeCreate() {},
+//生命周期 - 挂载之前
+beforeMount() {},
+ //生命周期 - 更新之前
+beforeUpdate() {},
+//生命周期 - 更新之后
+updated() {},
+//生命周期 - 销毁之前
+beforeDestroy() {},
+//生命周期 - 销毁完成
+destroyed() {},
+//如果页面有keep-alive缓存功能，这个函数会触发
+activated() {},
 }
 </script>
+
+<style lang='less' scoped>
+.tt-home-header {
+  height: 30px;
+  background-color: #000000;
+  line-height: 30px;
+  padding-left:5px;
+  span {
+    color:white;
+  }
+}
+.tt-home-content {
+   width:80vw;
+   margin-right: 10vw;
+   margin-left: 10vw;
+   display:flex;
+  .home-content-left {
+    flex:1;
+  }
+
+  .home-content-middle {
+    flex:3;
+  }
+
+  .home-content-right {
+    flex:2;
+  }
+}
+</style>
